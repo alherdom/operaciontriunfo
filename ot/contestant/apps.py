6e95ea926c84110ac9e9b7855bd1008d0ab969f3
alherdom <alherdom@outlook.com>
2023-11-30 22:24:46 +0000

    Edited: README.md

--- ot/contestant/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContestantConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contestant'
--- ot/teacher/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-
-from . import views
-
-app_name = "teacher"
-
-urlpatterns = [
-    path("list/", views.teacher_list, name="teacher_list"),
-]
