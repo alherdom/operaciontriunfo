e86955f12e0bed19d82da835d8e70b481b9d7756
Alejandro Hernández <alherdom@outlook.com>
2023-12-08 02:46:58 +0000

    Complete: all app details. ToDo: search input.

--- ot/contestant/views.py
+++ ot/contestant/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from .models import MusicStyle, Contestant
-
+from .forms import SearchForm
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "index.html")
@@ -14,3 +14,16 @@ def contestants(request: HttpRequest) -> HttpResponse:
 def contestant_detail(request: HttpRequest, contestant_slug: str) -> HttpResponse:
     contestant = Contestant.objects.get(slug=contestant_slug)
     return render(request, "contestant_detail.html", dict(contestant=contestant))
+
+def search(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            search = form.cleaned_data["search"]
+            contestants = Contestant.objects.filter(first_name__icontains=search)
+            return render(
+                request, "contestants.html", dict(contestants=contestants, search=search)
+            )
+    else:
+        form = SearchForm()
+    return render(request, "search.html", dict(form=form, contestants=contestants))
